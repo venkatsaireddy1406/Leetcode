710bb9e08e42a37f24df5bf1c13b3fb597788e5b
venkatsaireddy1406 <159175303+venkatsaireddy1406@users.noreply.github.com>
2024-02-07 01:23:06 +0530

    Update 217_Contains_Duplicate.py
    Added comments

--- Arrays & Hashing/217_Contains_Duplicate.py
+++ Arrays & Hashing/217_Contains_Duplicate.py
@@ -1,6 +1,6 @@
-def contains_duplicates(nums):
-    myset = set(nums)
-    return not len(nums) == len(myset)
+def contains_duplicates(nums): # Function to check for duplicates in list
+   myset = set(nums) # Create a set from list (duplicates removed)
+   return not len(nums == len(myset)) # Compare lengths of list and set
 
 if __name__=="__main__":
     print(contains_duplicates(nums = [1,2,3,1]))
