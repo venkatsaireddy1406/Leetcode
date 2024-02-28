578bc498e568ca8aad9384a692dd9d2252824408
venkatsaireddy1406 <159175303+venkatsaireddy1406@users.noreply.github.com>
2024-02-28 18:36:18 +0530

    Create 128_Longest_Consecutive_Sequence.py

---
+++ Arrays & Hashing/128_Longest_Consecutive_Sequence.py
@@ -0,0 +1,29 @@
+def main(nums):
+    """
+    Returns the length of the longest consecutive subsequence in the given list of numbers.
+
+    Args:
+    - nums: A list of integers
+
+    Returns:
+    - length: An integer representing the length of the longest consecutive subsequence
+    """
+
+    if not nums:
+        return 0
+
+    unique_nums = set(nums)
+    max_sequence_length = 0
+
+    for current_number in unique_nums:
+        if current_number - 1 not in unique_nums:
+            current_sequence = 1
+            while current_number + 1 in unique_nums:
+                current_number += 1
+                current_sequence += 1
+            max_sequence_length = max(max_sequence_length, current_sequence)
+
+    return max_sequence_length
+
+if  __name__ == "__main__":
+     print(main(nums = [100,4,200,1,3,2]))
